b1c9fd02888e8021a6dd656cdefcb9e2c114a660
emmadavid3662 <58483997+emmadavid3662@users.noreply.github.com>
2020-03-10 13:22:02 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+    public class Hello {
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Hello. What is your name? ");
+            String name = sc.next (); //answer the question, say your name
+            System.out.print("It's nice to meet you, " + name + ". How old are you? ");
+            String age = sc.next (); //say your age
+            System.out.println("I see that you are still quite young at only " + age + ". ");
+            System.out.print("Where do you live? ");
+            String home = sc.next (); //say where you live
+            System.out.print("Wow! I've always wanted to go to " + home + ". Thanks for chatting with me. Bye!");
+
+        }
+    }
